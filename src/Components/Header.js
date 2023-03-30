34eea9ebf3fcda8288e1201e9f326a5d1c7f9a5f
Namuun_L <mit.irnitu@gmail.com>
2023-03-30 11:51:42 +0800

    second

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,11 @@
 import { CarouselNav } from "./CarouselNav";
+import { Info } from "./info";
 
 export function Header() {
   return (
     <>
       <CarouselNav />
+      <Info/>
     </>
   );
 }
---
+++ src/Components/info.js
@@ -0,0 +1,17 @@
+import { CarouselNav } from "./CarouselNav";
+
+export function Info() {
+  return (
+    <>
+    <div className="container">
+        <div className="text-center">
+        <h1 className="text-center mt-5">Хүүхдийн ном</h1>
+        </div>
+        <div className="productSection">
+            
+        </div>
+    </div>
+    
+    </>
+  );
+}
