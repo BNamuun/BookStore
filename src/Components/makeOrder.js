f3d9c5ae5647e66739e473e668666cd38dc9e4fa
Namuun <mit.irnitu@gmail.com>
2023-04-25 23:46:35 +0800

    add bankAccount

--- src/Components/makeOrder.js
+++ src/Components/makeOrder.js
@@ -28,9 +28,9 @@ export function MakeOrder() {
     }
   }
   return (
-    <div className="max-w-screen-xl px-5 h-screen m-auto py-8">
+    <div className="max-w-screen-xl px-5 m-auto py-8">
       {/* <h2> Таны сагс</h2> */}
-      <div className="px-5 h-screen grid lg:grid-cols-2 gap-x-8">
+      <div className="grid lg:grid-cols-2 gap-x-10 min-h-screen">
         <div className="">
           {items.map((item) => (
             <>
@@ -88,18 +88,10 @@ export function MakeOrder() {
               ).format("0,0 ")}{" "}
               ₮
             </div>
-            <div>
-              <button
-                className="btn btn-primary"
-                onClick={() => setShowForm(true)}
-              >
-                {" "}
-                Захиалга хийх
-              </button>
-            </div>
           </div>
         </div>
-        <div className="order-form-container">
+
+        <div className="">
           <h4 className="text-lg font-semibold text-gray-900">
             Хүлээн авах хаяг
           </h4>
@@ -149,9 +141,47 @@ export function MakeOrder() {
             </div>
 
             <Button variant="primary" type="submit" className="w-60">
-              Submit
+              Захиалга хийх
             </Button>
           </Form>
+        </div>
+        <table class="border-double border-4 border-sky-800">
+          <tbody>
+            <tr className="border border-b-3">
+              <td className=" pl-3">Банк:</td>
+              <td className=" pl-3 font-bold">Хаан</td>
+            </tr>
+            <tr className="border border-b-3">
+              <td className=" pl-3">Хүлээн авагчийн нэр:</td>
+              <td className=" pl-3 font-bold">БатЭрдэнэ</td>
+            </tr>
+            <tr className="border border-b-3">
+              <td className=" pl-3">Данс:</td>
+              <td className=" pl-3 font-bold">5019716477</td>
+            </tr>
+            <tr className="border border-b-3">
+              <td className=" pl-3"> Дүн</td>
+              <td className=" pl-3 font-bold">
+                {" "}
+                {numeral(
+                  items.reduce((total, cartItem) => {
+                    const item = items.find((i) => i.id === cartItem.id);
+                    return total + (item?.price || 0) * cartItem.quantity;
+                  }, 0)
+                ).format("0,0 ")}{" "}
+                ₮
+              </td>
+            </tr>
+            <tr>
+              <td className=" pl-3"> Гүйлгээний утга:</td>
+              <td className=" pl-3 font-bold">Утасны дугаар</td>
+            </tr>
+          </tbody>
+        </table>
+        <div>
+          <span>
+            Та 24 цагийн дотор шилжүүлснээр таны захиалга амжилттай хийгдэнэ
+          </span>
         </div>
       </div>
     </div>
