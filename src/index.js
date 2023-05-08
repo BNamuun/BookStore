2ae5471dac19ba73abfe76efb73d6c4257aa630f
Namuun <mit.irnitu@gmail.com>
2023-05-08 15:24:18 +0800

    some

--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />;
-<head>
-  <meta property="og:title" content="wolf" />
-  <meta property="og:url" content="https://book-store-flame.vercel.app/" />
-  <meta property="og:type" content="article" />
-  <meta property="og:description" content="iim yum ban" />
-  <meta
-    property="og:image"
-    content="https://res.cloudinary.com/dkmzrowed/image/upload/v1683468942/public/level1/p7vz2u1jtqgs2glupzud.jpg"
-  />
-</head>;
+import Helmet from "react-helmet";
+<Helmet>
+  <head>
+    <meta property="og:title" content="Namuun" />
+    <meta property="og:url" content="https://book-store-flame.vercel.app/" />
+    <meta property="og:type" content="article" />
+    <meta property="og:description" content="iim yum ban" />
+    <meta
+      property="og:image"
+      content="https://res.cloudinary.com/dkmzrowed/image/upload/v1683468942/public/level1/p7vz2u1jtqgs2glupzud.jpg"
+    />
+  </head>
+</Helmet>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
